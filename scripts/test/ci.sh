2d984cc52572fc322abfde86a3909808c790510b
Michael North <michael.l.north@gmail.com>
2018-01-08 17:53:29 -0800

    Test suites

--- scripts/test/ci.sh
+++ scripts/test/ci.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-test/posix.sh && npm run lint:ts && node_modules/.bin/mocha -o test/mocha.opts
+echo "TEST_SUITE=$TEST_SUITE"
+if [ "$TEST_SUITE" == "SHELL" ]; then
+  test/posix.sh
+elif [ "$TEST_SUITE" == "LINT" ]; then
+  npm run lint:ts
+elif [ "$TEST_SUITE" == "UNIT" ]; then
+  node_modules/.bin/mocha -o test/mocha.opts
+else
+  echo "no TEST_SUITE defined"
+  exit 1
+fi
