71edfc5edec18f5a7c18b9c0b9d76946ec5afdd2
Michael North <michael.l.north@gmail.com>
2018-01-09 19:59:02 -0800

    More logging in CI

--- scripts-ci/install.sh
+++ scripts-ci/install.sh
@@ -6,6 +6,7 @@ NODE_VERSION=$($(which node) -v)
 echo "NODE_VERSION=$NODE_VERSION"
 semverGT $NODE_VERSION "4.0.0"
 NODE_VERSION_CHECK_RESULT=$?
+echo "NODE_VERSION_CHECK_RESULT=$NODE_VERSION_CHECK_RESULT"
 if [ "$NODE_VERSION_CHECK_RESULT" == "1" ]; then
   $(which npm) install
 else
