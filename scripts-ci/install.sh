ca17dd6c0038310ef382621bb19193f04e895119
Michael North <michael.l.north@gmail.com>
2018-01-09 20:00:17 -0800

    Flip npm install condition

--- scripts-ci/install.sh
+++ scripts-ci/install.sh
@@ -8,8 +8,8 @@ semverGT $NODE_VERSION "4.0.0"
 NODE_VERSION_CHECK_RESULT=$?
 echo "NODE_VERSION_CHECK_RESULT=$NODE_VERSION_CHECK_RESULT"
 if [ "$NODE_VERSION_CHECK_RESULT" == "1" ]; then
-  $(which npm) install
-else
   echo "Skipping npm install"
+else
+  $(which npm) install
 fi
 # if ($NO)
