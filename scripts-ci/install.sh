118fc2c58e0690f6c91584cf78e644e21713763f
Michael North <michael.l.north@gmail.com>
2018-01-09 20:02:10 -0800

    Don't npm install for bootstrap tests

--- scripts-ci/install.sh
+++ scripts-ci/install.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-. ./bootstrap/semver.sh
-
-NODE_VERSION=$($(which node) -v)
-echo "NODE_VERSION=$NODE_VERSION"
-semverGT $NODE_VERSION "4.0.0"
-NODE_VERSION_CHECK_RESULT=$?
-echo "NODE_VERSION_CHECK_RESULT=$NODE_VERSION_CHECK_RESULT"
-if [ "$NODE_VERSION_CHECK_RESULT" == "1" ]; then
+if [ "$TEST_SUITE" == "SHELL" ]; then
   echo "Skipping npm install"
 else
   $(which npm) install
